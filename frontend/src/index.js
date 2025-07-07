df46f7ecc7d9c7a2bf6468e26c39af4504be49f6
Andres Suarez <23503930+glicerico@users.noreply.github.com>
2025-07-07 14:19:05 -0600

    First attempt at dark mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './styles/theme.css';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
